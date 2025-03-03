d0e26dda562e52015da069bef6b0e75163f6de08
OmkarDhavalagi <omkardhavalagi@gmail.com>
2025-03-03 22:19:20 +0530

    added getElementById code

---
+++ getElementById.js
@@ -0,0 +1,10 @@
+let heading = document.getElementById("main-heading");
+heading.textContent = "Fruit World";
+heading.style.color = "orange";
+let heading2 = document.getElementById("header");
+heading2.style.backgroundColor = "green";
+heading2.style.borderBottom = "orange";
+let heading3 = document.getElementById("basket-heading");
+heading3.style.color = "green";
+let regards = document.getElementById("thanks");
+regards.innerHTML = "<p>Please visit us again</p>";
